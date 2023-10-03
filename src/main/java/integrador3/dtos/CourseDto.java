712c6072c1b155b515a29bf742b022ad056bfa56
jose-acosta-git <jose.acosta.git@gmail.com>
2023-10-03 17:13:37 -0300

    Add StudentDto

---
+++ src/main/java/integrador3/dtos/CourseDto.java
@@ -0,0 +1,13 @@
+package integrador3.dtos;
+
+public class CourseDto {
+
+	private String name;
+	
+	public CourseDto(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public String getName() { return this.name; }
+}
