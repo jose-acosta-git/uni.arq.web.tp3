017af901b114ef58e924eb4692b31cb24d812483
jose-acosta-git <jose.acosta.git@gmail.com>
2023-10-03 16:53:12 -0300

    Add StudentService

---
+++ src/main/java/integrador3/services/StudentService.java
@@ -0,0 +1,29 @@
+package integrador3.services;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import integrador3.dtos.StudentDto;
+import integrador3.model.Student;
+import integrador3.repositories.StudentRepository;
+
+@Service
+public class StudentService {
+
+	@Autowired
+    private StudentRepository studentRepository;
+	
+	public List<StudentDto> findAll() {
+		List<Student> students = studentRepository.findAll();
+		List<StudentDto> dtoList = new LinkedList<>();
+		students.forEach(s -> dtoList.add(convertToDto(s)));
+		return dtoList;
+	}
+	
+	private StudentDto convertToDto(Student s) {
+		return new StudentDto(s.getDni(), s.getName(), s.getLast_name(), s.getAge(), s.getGender(), s.getCity(), s.getNotebook_number());
+	}
+}
