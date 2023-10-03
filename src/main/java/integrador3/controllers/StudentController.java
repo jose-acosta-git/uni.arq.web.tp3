a748d637836fb2844349436529bbdfda6fb83f12
jose-acosta-git <jose.acosta.git@gmail.com>
2023-10-03 16:53:23 -0300

    Add StudentController

---
+++ src/main/java/integrador3/controllers/StudentController.java
@@ -0,0 +1,35 @@
+package integrador3.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import integrador3.dtos.StudentDto;
+import integrador3.model.Student;
+import integrador3.repositories.StudentRepository;
+import integrador3.services.StudentService;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+	@Autowired
+	private StudentRepository studentRepository;
+	@Autowired
+	private StudentService studentService;
+	
+	@GetMapping
+	public List<StudentDto> getAll() {
+		return studentService.findAll();
+	}
+	
+	@PostMapping
+	public Student create(@RequestBody Student student) {
+		return studentRepository.save(student);
+	}
+}
